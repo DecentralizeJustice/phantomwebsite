450283cd0de21bc54f4ca07c6334a8428360daa1
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-04 16:53:29 -0400

    reuse connection

---
+++ netlify/functions/getDemoTexts.js
@@ -0,0 +1,42 @@
+const mongoDBPassword = process.env.mongoDBPassword
+const mongoServerLocation = process.env.mongoServerLocation
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri = "mongodb+srv://main:" + mongoDBPassword + "@"+ mongoServerLocation + "/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+exports.handler = async (event) => {
+  try {
+    const textObjectDirty = await getText()
+    const cleanTexts = []
+    for (const element of textObjectDirty.messageArray) {
+        let holderObject = {}
+        holderObject.text = element.text
+        holderObject.sentStamp = element.sentStamp
+        holderObject.receivedStamp = element.receivedStamps
+        holderObject.from = element.from.slice(0, -4) + 'XXXX'
+        cleanTexts.push(holderObject)
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(cleanTexts)
+      } 
+  } 
+  catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: ''
+    }
+  }
+}
+
+async function getText(){
+  const collection = client.db("demo").collection("demo")
+  // const docInfo = {_id: 69, messageArray: [] }
+  // await collection.insertOne(docInfo)
+  const test = await collection.findOne(
+    { _id: 69 }, { projection : { messageArray: { $slice: -50 } }}
+  )
+ // await client.close()
+  return test
+ 
+}
