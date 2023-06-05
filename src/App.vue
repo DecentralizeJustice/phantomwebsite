45d8ac04e9d2fe12ae904d0b722193881931a993
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-05 11:30:42 -0400

    correct day

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ function localTime(epoch) {
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'pm' : 'am';
-  var day =  date.getDate();
+  var day =  date.getDate() + 1
   var month = date.getMonth()
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
