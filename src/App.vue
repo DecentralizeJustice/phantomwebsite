7e61c8af320c9c2a70ecca2a3c42337787282d9d
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-04 21:48:59 -0400

    slight improvments

--- src/App.vue
+++ src/App.vue
@@ -307,4 +307,7 @@ document.addEventListener('DOMContentLoaded', function() {
 .bg-gradient-radial-dark-light {
   background-image:radial-gradient(50% 50% at 50% 50%,rgba(31,41,55,0) 0,#1f2937 100%)
 }
+.bg-gradient-radial-dark {
+  background-image:radial-gradient(50% 50% at 50% 50%,rgba(17,24,39,0) 0,#111827 100%)
+}
 </style>
