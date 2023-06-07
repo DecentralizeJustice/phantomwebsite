196e776602d37a44fc2fbe924ab09cb6b630ae86
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-07 00:02:03 -0400

    10 a month

--- src/App.vue
+++ src/App.vue
@@ -266,7 +266,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class="w-full md:w-1/3 p-4">
           <div class="flex flex-col justify-between p-8 h-full bg-gray-900 rounded-3xl">
             <div class="flex-initial mb-6"><span class="inline-block mb-6 text-sm text-blue-500 font-bold uppercase tracking-widest">BASIC 3 Month</span>
-              <h2 class="font-heading text-4xl text-white font-black tracking-tight">$12/mo</h2>
+              <h2 class="font-heading text-4xl text-white font-black tracking-tight">$10/mo</h2>
               <p class="mb-8 text-sm text-gray-700 font-bold">Billed 3 Months At A Time</p>
               <ul><li class="flex items-center mb-4">
                 <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="text-gray-100 font-bold">60 Texts a Month</p>
