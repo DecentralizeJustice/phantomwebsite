6cfeb44d4d61b431487cc00d4521d9c7351594b9
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-08 14:43:08 -0400

    created checkout page

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -5,18 +5,40 @@
 <template>
 <section class="py-10 bg-gray-800 overflow-hidden">
   <div class="container mx-auto px-4">
-    <div class="relative py-20 px-8 bg-gray-900 overflow-hidden rounded-3xl">
-      <div class="absolute top-1/2 left-1/2 min-w-max transform -translate-x-1/2 -translate-y-1/2">
+    <div class="relative p-10 bg-gray-900 overflow-hidden rounded-3xl">
+<!--       <div class="absolute top-1/2 left-1/2 min-w-max transform -translate-x-1/2 -translate-y-1/2">
         <div class="absolute bg-gradient-radial-dark w-full h-full"></div><img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1685928443/landingpage/pattern-dark.png" alt="">
-      </div>
-      <div class="relative z-10 pb-32 md:pb-64 max-w-7xl mx-auto">
-        <div class="md:max-w-md mx-auto text-center"><img class="mb-32 md:mb-64 mx-auto" src="" alt="">
-          <h3 class="font-heading text-2xl text-blue-500 font-black tracking-tight">404</h3>
-          <h2 class="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-white font-black tracking-tight">Page not found</h2>
-          <p class="mb-6 text-gray-500 font-bold">We’re sorry, the page you requested could not be found. Please go back to the homepage</p>
-          <div class="flex flex-wrap justify-center -m-2">
+      </div> -->
+      <div class="relative z-10">
+        <div class="flex flex-wrap items-center -m-8">
+          <div class="w-full md:w-1/2 p-8"><img src="https://res.cloudinary.com/dylevfpbl/image/upload/v1686249628/landingpage/image_4.png" alt=""></div>
+          <div class="w-full md:w-1/2 p-8 order-first md:order-last">
+            <div class="md:max-w-md mx-auto text-center">
+            
+        <span class="inline-block mb-6 text-lg text-blue-500 font-bold uppercase tracking-widest">Purchase PREMIUM 3 Month</span>
+          <h2 class="font-heading text-6xl text-white font-black tracking-tight">$20/mo</h2>
+              <p class="mb-8 text-lg text-gray-700 font-bold">Billed 3 Months At A Time</p>
+              <ul><li class="flex items-center mb-4">
+                <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="text-gray-100 font-bold text-xl">Unlimited Texts*</p>
+                </li>
+                <li class="flex items-center mb-4">
+                  <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="text-gray-100 font-bold text-xl">Web Gui Access</p>
+                </li>
+                <li class="flex items-center mb-4">
+                  <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="text-gray-100 font-bold text-xl">Premium Support</p>
+                </li>
+                <li class="flex items-center mb-4">
+                  <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="font-bold text-gray-100 text-xl">Extra Text Available for Purchase</p>
+                </li>
+                <li class="flex items-center mb-4">
+                  <svg class="mr-2.5" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.16699 10.8333L7.50033 14.1667L15.8337 5.83333" stroke="#3B82F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg><p class="font-bold text-gray-100 text-xl">Access to Early Features First</p>
+                </li>
+              </ul>
+              <div class="flex flex-wrap justify-center -m-2 mt-5">
             <div class="w-full md:w-auto p-2"><a class="block w-full px-4 py-2.5 text-sm text-center text-white font-bold bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-200 rounded-full" href="#">Go back to Homepage</a></div>
-            <div class="w-full md:w-auto p-2"><a class="block w-full px-4 py-2.5 text-sm text-center text-gray-900 font-bold bg-gray-50 hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 rounded-full" href="#">Try Again</a></div>
+          </div>
+
+            </div>
           </div>
         </div>
       </div>
