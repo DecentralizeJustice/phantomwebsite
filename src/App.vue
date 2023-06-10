bdf05af9688bb0529f2511a7bbd14c54b322c801
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-09 23:04:31 -0400

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import headerCustom from "./components/header.vue"
+import footerCustom from "./components/footer.vue"
 </script>
 
 <template>
 <headerCustom />
 <router-view/>
+<footerCustom/>
 </template>
 
 <style scoped>
