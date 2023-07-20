e6e3728154d6fa581f628f26d43d616355b3a098
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-07-20 10:36:07 -0400

    sold out

--- src/views/landingpage.vue
+++ src/views/landingpage.vue
@@ -184,7 +184,7 @@ const messageArray = ref([])
               </ul></div>
             <div class="flex-initial">
               <div class="flex flex-wrap -m-2">
-                <div class="w-full p-2"><router-link class="block w-full px-8 py-3.5 text-lg text-center text-gray-100 font-bold bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-400 rounded-full" to="/checkout1service">Purchase</router-link></div>
+                <div class="w-full p-2"><router-link class="block w-full px-8 py-3.5 text-lg text-center text-gray-100 font-bold bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-400 rounded-full" to="/">Sold Out</router-link></div>
               </div>
             </div>
           </div>
